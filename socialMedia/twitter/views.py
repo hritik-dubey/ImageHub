955ca2a0f6112c441987cc83caf3e282bbd7e546
hritikdubey-22 <hritik.dubey@polynomial.a>
2024-06-16 20:56:40 +0530

    completed till the models

---
+++ socialMedia/twitter/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404,redirect
+# Create your viewsk here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet  = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save(commit=True)
+            return redirect('tweet_list')
+    else:
+        from = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+def tweet_update(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id ,user=request.user)
+    if request.method == 'POST':
+        from  = TweetForm(request.POST, request.FILES)
+        if from.is_valid():
+            tweet.user = request.user
+            tweet.save(commit=True)
+            return redirect('tweet_list')
+    else:
+        from = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': from})
+
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id ,user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'tweet_delete.html', {'tweet': tweet})
